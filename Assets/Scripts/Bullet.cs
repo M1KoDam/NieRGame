9ba15e3bd3d75a3187b1339d82625590d93bd1a5
L0veR1ck <lavr.rar@yandex.ru>
2023-04-18 21:37:19 +0500

    add fire rate and changed bullets deaths

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -1,5 +1,4 @@
 using UnityEngine;
-using UnityEngine.Serialization;
 
 public class Bullet : MonoBehaviour
 {
@@ -7,18 +6,26 @@ public class Bullet : MonoBehaviour
     private Camera _camera;
     public int bulletSpeed;
     
-    private Vector2 BulletToMouse => (_camera.ScreenToWorldPoint(Input.mousePosition) - _rb.transform.position);
+    private Vector2 BulletToMouse => _camera.ScreenToWorldPoint(Input.mousePosition) - _rb.transform.position;
     
     private void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
         _camera = Camera.main;
-
+        
         Accelerate();
     }
     
     private void Accelerate()
     {
         _rb.velocity = bulletSpeed * BulletToMouse.normalized;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            Destroy(gameObject);
+        }
     }
 }

7de4c32b1f7fbff0c462714483006bce3b0a1563
dimastarsis <kurilin.dmitriy@gmail.com>
2023-05-15 23:08:56 +0500

    add wall

--- Assets/Scripts/SmallFlyer.cs
+++ Assets/Scripts/SmallFlyer.cs
@@ -146,7 +146,7 @@ public class SmallFlyer : MonoBehaviour
         
     }
 
-private void Patrolling()
+    private void Patrolling()
     {
         _isScoping = false;
         if (SmallFlyerToSpot.magnitude < 1f)

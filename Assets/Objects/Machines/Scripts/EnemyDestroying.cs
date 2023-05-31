fb3c4151e058ebbc8e1b1ada0e499b83ad1ed588
L0veR1ck <lavr.rar@yandex.ru>
2023-05-31 22:11:24 +0500

    enemy destroying fixes on flight top

--- Assets/Objects/Machines/Scripts/EnemyDestroying.cs
+++ Assets/Objects/Machines/Scripts/EnemyDestroying.cs
@@ -1,17 +1,31 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EnemyDestroying : MonoBehaviour
 {
+    private bool _isActive;
+    
     public void Activate()
     {
         foreach(var childBody in GetComponentsInChildren<Rigidbody2D>())
         {
             childBody.bodyType = RigidbodyType2D.Dynamic;
             childBody.mass = 5f;
+            var scene = SceneManager.GetActiveScene();
+            if (scene.name == "FlightTop")
+            {
+                childBody.gravityScale = 0;
+                _isActive = true;
+            }
         }
+    }
+
+    private void FixedUpdate()
+    {
+        if (!_isActive)
+            return;
+
+        if (((Vector2)transform.localScale).magnitude >= 0.01f)
+            transform.localScale -= new Vector3(0.005f, 0.005f, 0);
     }
 }

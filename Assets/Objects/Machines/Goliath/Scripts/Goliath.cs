1da8f681bf5fcc0e6ccfd01b8c9a950151c9ee6b
L0veR1ck <lavr.rar@yandex.ru>
2023-06-01 22:00:19 +0500

    hello i am goliath

--- Assets/Objects/Machines/Goliath/Scripts/Goliath.cs
+++ Assets/Objects/Machines/Goliath/Scripts/Goliath.cs
@@ -1,8 +1,48 @@
 using System;
+using Unity.Burst.Intrinsics;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Goliath : Enemy
 {
+    [SerializeField] private float speed;
+    [SerializeField] private Transform[] platformLevels;
+    [SerializeField] private Transform armRight;
+    [SerializeField] private GameObject handRight;
+    [SerializeField] private Transform elbowRight;
+    [SerializeField] private GameObject fistRight;
+    [SerializeField] private float armRotationSpeed;
+    [SerializeField] private float handRotationSpeed;
+    private int _currentPlatformLevel;
+    private Vector3? target;
+
+    private const float IdleArmAngle = 0;
+    private const float IdleHandAngle = 0;
+
+    private const float SwingArmAngle = -150;
+    private const float SwingHandAngle = -37;
+
+    private const float KickArmAngle = -30;
+    private const float KickHandAngle = 10;
+
+    private const int AttackEndStatusCode = 228;
+
+    private float currentArmAngle;
+    private float currentHandAngle;
+
+    private int attackStatus = AttackEndStatusCode;
+    private int currentAttackType;
+
+    private float timer;
+
+    private float targetArmAngle { get; set; }
+    private float targetHandAngle { get; set; }
+
+    private Vector3? vectorToTarget => (target - transform.position)?.normalized;
+    private bool HandOnTarget => Math.Abs(currentHandAngle - targetHandAngle) <= handRotationSpeed;
+    private bool ArmOnTarget => Math.Abs(currentArmAngle - targetArmAngle) <= armRotationSpeed;
+    private bool BothOnTarget => HandOnTarget && ArmOnTarget;
+
     protected override IState State
         => hp <= 0
             ? new DeadState()
@@ -10,7 +50,7 @@ public class Goliath : Enemy
 
     protected override void Start()
     {
-        base.Start();
+        target = platformLevels[2].position;
     }
 
     protected void Update()
@@ -19,9 +59,42 @@ public class Goliath : Enemy
             return;
     }
 
+    private void HandleHandMovement()
+    {
+        if (!ArmOnTarget)
+        {
+            var currentArmSpeed = currentArmAngle > targetArmAngle ? -armRotationSpeed : armRotationSpeed;
+            handRight.transform.RotateAround(armRight.position, Vector3.forward, currentArmSpeed);
+            currentArmAngle += currentArmSpeed;
+        }
+
+        if (!HandOnTarget)
+        {
+            var currentHandSpeed = currentHandAngle > targetHandAngle ? -handRotationSpeed : handRotationSpeed;
+            fistRight.transform.RotateAround(elbowRight.position, Vector3.forward, currentHandSpeed);
+            currentHandAngle += currentHandSpeed;
+        }
+    }
+
+    private void HandleMovement()
+    {
+        if (vectorToTarget is null)
+            return;
+
+        var position = transform.position;
+        position.y += vectorToTarget.Value.y * speed;
+        transform.position = position;
+    }
+
     protected void FixedUpdate()
     {
         State.Execute(this);
+
+        HandleMovement();
+        HandleHandMovement();
+
+        // Debug.Log($"HAND: current:{currentHandAngle}, target:{targetHandAngle}");
+        // Debug.Log($"ARM: current:{currentArmAngle}, target:{targetArmAngle}");
     }
 
     public override void Patrol()
@@ -36,7 +109,76 @@ public class Goliath : Enemy
 
     public override void Attack()
     {
-        throw new NotImplementedException();
+        if (attackStatus == AttackEndStatusCode)
+        {
+            currentAttackType = Random.Range(1, 2);
+            attackStatus = 0;
+        }
+        
+        if (currentAttackType == 1)
+            AttackType1();
+        
+        if (currentAttackType == 2)
+            AttackType2();
+    }
+
+    private void AttackType2()
+    {
+        attackStatus = AttackEndStatusCode;
+    }
+
+    private void AttackType1()
+    {
+        if (attackStatus == 0)
+            Swing();
+        if (attackStatus == 1)
+            Wait(2);
+        if (attackStatus == 2)
+            Kick();
+        if (attackStatus == 3)
+            Wait(2);
+        if (attackStatus == 4)
+            Idle();
+        if (attackStatus == 5)
+            attackStatus = AttackEndStatusCode;
+    }
+
+    private void Wait(int seconds)
+    {
+        timer += Time.fixedDeltaTime;
+
+        if (timer >= seconds)
+        {
+            timer = 0;
+            attackStatus++;
+        }
+    }
+
+    private void Swing()
+    {
+        targetArmAngle = SwingArmAngle;
+        targetHandAngle = SwingHandAngle;
+
+        if (BothOnTarget)
+            attackStatus++;
+    }
+
+    private void Kick()
+    {
+        targetArmAngle = KickArmAngle;
+        targetHandAngle = KickHandAngle;
+        
+        if (BothOnTarget)
+            attackStatus++;
+    }
+
+    private void Idle()
+    {
+        targetArmAngle = IdleArmAngle;
+        targetHandAngle = IdleHandAngle;
+        
+        if (BothOnTarget)
+            attackStatus++;
     }
 
     public override void GoToScene()
@@ -52,5 +194,10 @@ public class Goliath : Enemy
     public override void Die()
     {
         throw new NotImplementedException();
+    }
+
+    public void MoveHead(int transformLevel)
+    {
+        target = platformLevels[transformLevel].position;
     }
 }

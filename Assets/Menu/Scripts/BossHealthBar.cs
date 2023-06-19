192ecd496e2b6feee7ed576c299fb5b68a918715
L0veR1ck <lavr.rar@yandex.ru>
2023-06-19 23:02:54 +0500

    goliath borders fixes

--- Assets/Menu/Scripts/BossHealthBar.cs
+++ Assets/Menu/Scripts/BossHealthBar.cs
@@ -19,7 +19,6 @@ public class BossHealthBar : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        Debug.Log(_slider.value);
         _slider.value = goliath.GetHealth();
     }
 }
